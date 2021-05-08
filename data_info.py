ea57633a1e3b17f2f92201dc04c03b1531d0cd73
Eden A <eden.a.mcewen@gmail.com>
2021-05-08 01:27:30 -0700

    final updates

--- data_info.py
+++ data_info.py
@@ -6,10 +6,32 @@ from scipy import signal
 from scipy.io import wavfile
 import numpy as np
 import pandas as pd
+import scipy.optimize as opt
+
+# check how bottles changed over measurements
+d_water = 997e3 # g/m³
+# 0 - bottle weight
+# 1 - neck volume
+# 2 - total volume
+
+b12 = [[458, 454, 454, 456],
+       [464, 472, 474, 472],
+       [514, 518, 518, 518]]
+
+b5 = [[452, 452, 450],
+      [472, 476, 474],
+      [518, 518, 518]]
+
+b2 = [[450, 448, 450],
+      [472, 476, 472],
+      [520, 520, 520]]
+
+mean_neck = np.average(b12[1])
+mean_top = np.average(b12[2])
 
 calib_f = np.array([100, 150, 200, 250, 300, 350, 400, 450, 500])
 data_m = np.array([0, 56, 112, 152, 200, 250, 300, 356, 406, 470])
-cold = []
+cold = np.array([0, 54, 118, 150, 206, 254, 306, 356, 402, 452, 470])
 
 def csv_fq(index):
     #given the index of a file
@@ -57,3 +79,96 @@ def f_max(csv_f):
     time_fvg = np.average(spec_flted, axis = 1)
     max_ffeq = np.argmax(time_fvg) # index of max peak
     return max_ffeq
+
+
+# Chi squared goodness of fit
+def hand_chisquared(ms, exp, unc):
+    ms = np.array(ms)
+    exp = np.array(exp)
+    return np.sum(((ms - exp)/unc)**2)
+
+def hand_chisquared_r(ms, exp, unc, df):
+    ms = np.array(ms)
+    exp = np.array(exp)
+    return np.sum(((ms - exp)/unc)**2)/df
+
+### FITS
+
+#### Ply fit:
+
+def Poly(x, a, b):
+    return b*x**a
+
+def Poly_est(x, a, b):
+    #returns y estimates
+    y_est = [Poly(xi, a, b) for xi in x]
+    return y_est
+
+def Poly_fit(x, y):
+    # returns [slope, intercept]
+    # the optimized curve returns the optimized abc values for Gaussian
+    # [height, position of peak center (mean), standard deviation]
+    return opt.curve_fit(Poly, x, y)
+
+# Linear Fits
+
+def Linear_tot(x, y):
+    a, b = Linear_Fit(x, y)
+    a_err, b_err = Linear_err(x, y, a, b)
+    return np.array([a, a_err, b, b_err])
+
+def Linear(x, a, b):
+    return a * x + b
+
+def Linear_Fit(x, y):
+    # returns [slope, intercept]
+    # the optimized curve returns the optimized abc values for Gaussian
+    # [height, position of peak center (mean), standard deviation]
+    popt, pcov = opt.curve_fit(Linear, x, y)
+    return popt[0], popt[1]
+    
+def Linear_fit(x, y):
+    # returns fit
+    return opt.curve_fit(Linear, x, y)
+    
+def Linear_est(x, a, b):
+    #returns y estimates
+    y_est = [Linear(xi, a, b) for xi in x]
+    return y_est
+
+#### Uncertainties of fit
+
+def Linear_err(x, y, a, b):
+    cu = com_uncert(x, y, a, b)
+    b_err = err_intercept(x, y, cu)
+    a_err = err_gradient(x, y, cu)
+    return a_err, b_err
+
+# Main TWO
+
+def err_intercept(x, y, cu):
+    # calculates error in liner fit intercepts
+    x = np.array(x)
+    dlt = delta(x)
+    return cu * np.sqrt(np.sum(x**2)/dlt)
+
+def err_gradient(x, y, cu):
+    # calculates error in gradient
+    x = np.array(x)
+    dlt = delta(x)
+    return cu * np.sqrt(x.shape[0]/dlt)
+
+# Helper Functions
+
+def com_uncert(x, y, a, b):
+    # calculates the common uncertainty
+    x = np.array(x)
+    y = np.array(y)
+    diff_array = (y - a * x - b)**2
+    diff = np.sum(diff_array)
+    return np.sqrt((1/(x.shape[0]-2))*diff)
+
+def delta(x):
+    # helper function
+    x = np.array(x)
+    return x.shape[0]*np.sum(x**2) - np.sum(x)**2
